91a7af1f8a2a389b5968b16aeb17761901f23a36
Pocket Of Weird <hartzlernathan@gmail.com>
2020-04-16 13:31:35 -0500

    May remove webview

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+extern crate csv;
 extern crate serde;
 extern crate serde_json;
 extern crate tinyfiledialogs as tfd;
@@ -6,29 +7,93 @@ extern crate web_view;
 use serde::{Serialize, Deserialize};
 use tfd::MessageBoxIcon;
 use web_view::{Content};
+use std::collections::HashMap;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 struct Task {
     command: String,
     data: String
 }
-
-fn print_file(file: String) {
-    println!("file: {}", file);
+#[derive(Debug, Deserialize)]
+struct Order {
+    #[serde(rename = "Order #")]
+    id: u32,
+    #[serde(rename = "Fulfillment Type")]
+    fulfillment: Option<String>,
+    #[serde(rename = "Paid")]
+    paid: String,
+    #[serde(rename = "Item")]
+    item: String,
+    #[serde(rename = "Unit Size")]
+    unit_size: Option<String>,
+    #[serde(rename = "Quantity")]
+    quantity: Option<u32>,
+    #[serde(rename = "Price")]
+    price: Option<String>,
+    #[serde(rename = "Customer")]
+    customer: String,
+    #[serde(rename = "Pickup Address")]
+    addr_pickup: Option<String>,
+    #[serde(rename = "Delivery Address")]
+    addr_delivery: Option<String>,
 }
 
-fn open_file() {
-    match tfd::open_file_dialog("Please choose a csv file...", "", None) {
-        Some(path) => tfd::message_box_ok("File chosen", &path, MessageBoxIcon::Info),
-        None => tfd::message_box_ok(
-            "Warning",
-            "You didn't choose a file.",
-            MessageBoxIcon::Warning,
-        ),
+fn process_csv(path: String) -> Option<HashMap<u32, Vec<Order>>> {
+    let rdr = csv::Reader::from_path(&path);
+    if rdr.is_ok() {
+        let mut orders: HashMap<u32, Vec<Order>> = HashMap::new();
+        for result in rdr.unwrap().deserialize() {
+            let order: Order = result.unwrap();
+            if orders.contains_key(&order.id) {
+                let items = orders.get_mut(&order.id).unwrap();
+                items.push(order);
+            } else {
+                orders.insert(order.id, vec![order]);
+            }
+        }
+        return Some(orders);
+    } else {
+        tfd::message_box_ok("Error", &format!("Error opening the file: {}", path), MessageBoxIcon::Error);
+        return None;
     }
 }
 
+fn create_pdf(path: String, orders: HashMap<u32, Vec<Order>>) {
+    
+    tfd::message_box_ok("Complete", "The Dymo Labels for the B2D Orders have been created.", MessageBoxIcon::Info);
+}
+
+fn file_warning() {
+    tfd::message_box_ok("Warning", "You didn't choose a file.", MessageBoxIcon::Warning);
+}
+
+fn save_file() -> Option<String> {
+    return tfd::save_file_dialog("Save the labels PDF file...", "");
+}
+
+
+fn open_file() -> Option<String> {
+    return tfd::open_file_dialog("Please choose a Barn2Door csv file...", "", None);
+}
+
 fn main() {
+    let file_path = open_file();
+    if file_path.is_some() {
+        let orders = process_csv(file_path.unwrap());
+        if orders.is_some() {
+            let save_path = save_file();
+            if save_path.is_some() {
+                create_pdf(save_path.unwrap(), orders.unwrap());
+            } else {
+                file_warning();
+            }
+        } else {
+            tfd::message_box_ok("Error", "Error processing the csv file", MessageBoxIcon::Error);    
+        }
+    } else {
+        file_warning();
+    }
+    /*
     web_view::builder()
         .title("B2D2")
         .content(Content::Html(include_str!("static/index.html")))
@@ -39,7 +104,6 @@ fn main() {
             let task: Task = serde_json::from_str(arg).unwrap();
 
             match task.command.as_str() {
-                "add_file" => print_file(task.data),
                 "open" => open_file(),
                 _ => (),
             }
@@ -48,5 +112,6 @@ fn main() {
         })
         .run()
         .unwrap();
+    */
 }
 
